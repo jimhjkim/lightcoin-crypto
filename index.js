2797c60f8df90be688ba25d3f7a859bce7ae0c57
Jim Kim <jim.hyunjin.kim@gmail.com>
2019-09-27 16:42:04 -0400

    updated with features

--- index.js
+++ index.js
@@ -1,29 +1,63 @@
-let balance = 500.00;
-
-class Withdrawal {
-
-  constructor(amount) {
-    this.amount = amount;
+// let balance = 500.00;
+class Account {
+  constructor(username) {
+    this.username = username;
+    this.transactions = [];
   }
-
-  commit() {
-    balance -= this.amount;
+  get balance() {
+    let balance = 0;
+    for (let t of this.transactions) {
+      balance += t.value;
+    }
+    return balance;
   }
-
+  addTransaction(transaction) {
+    this.transactions.push(transaction);
+  }
 }
 
+class Transaction {
+  constructor(amount, account) {
+    this.amount = amount;
+    this.account = account;
+  }
+  commit() {
+    if (this.isAllowed()) {
+      this.time = new Date(); // keep track of the time of the transaction
+      this.account.addTransaction(this); // add the transaction to the account
+      return `Withdrawal of $${this.amount} was Successful`;
+    } return `Withdrawal of $${this.amount} was Unsuccessful`;
+  }
+}
 
+class Deposit extends Transaction {
+  get value() {
+    return this.amount;
+  }
+  isAllowed() {
+    return true; // no need to check
+  }
+}
 
+class Withdrawal extends Transaction {
+  get value() {
+    return -this.amount;
+  }
+  isAllowed() {
+    return this.account.balance - this.amount >= 0;
+  }
+}
 
 // DRIVER CODE BELOW
 // We use the code below to "drive" the application logic above and make sure it's working as expected
+const myAccount = new Account('snow-patrol');
 
-t1 = new Withdrawal(50.25);
-t1.commit();
-console.log('Transaction 1:', t1);
+console.log('Starting Balance', myAccount.balance);
 
-t2 = new Withdrawal(9.99);
-t2.commit();
-console.log('Transaction 2:', t2);
+const t1 = new Deposit(120.00, myAccount);
+console.log(t1.commit());
 
-console.log('Balance:', balance);
+const t2 = new Withdrawal(500.00, myAccount);
+console.log(t2.commit());
+
+console.log('Ending Balance:', myAccount.balance);
